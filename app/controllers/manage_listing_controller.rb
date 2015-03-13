c8ecc47a77a2ffcbb03ef32737ffa1c5e29d6270
Carl Tanner <carl@wdwhub.net>
2015-03-13 10:27:49 -0700

    added config info for manage_lisitng_controller show for listing info

--- app/controllers/manage_listing_controller.rb
+++ app/controllers/manage_listing_controller.rb
@@ -5,6 +5,33 @@ class ManageListingController < ApplicationController
   def show
     @space    = Space.find(params[:space_id])
     @photos   = @space.photos
+    
+    ## Listing page is divided into two sections options. The OpenStruct feeds options.
+    # Rooms and Beds
+    rooms_n_beds = {bedrooms: [*1..10], 
+                    beds: [*1..15], 
+                    bed_style: ["airbed","futon","pull_out_sofa","couch","real_bed"],
+                    bathrooms: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5]
+                  }
+
+    # Listing Info
+    # home_style is huge, so we break it down
+    home_style_keys         = [*1..25]
+    home_style_description  = ["Apartment", "House", "Bed &amp; Breakfast", "Condo", "Loft", "Cabin", "Villa", "Castle",
+                              "Dorm", "Treehouse", "Boat", "Plane", "Camper/RV", "Igloo", "Lighthouse", "Yurt", "Tipi",
+                              "Cave", "Island", "Chalet", "Earth House", "Hut", "Train", "Tent", "Other"
+                            ]
+    home_style              = Hash[*keys.zip(home_styles).flatten]
+    
+    # roome_style and person capacity are simple
+    room_style  =  {"entire_home" => "Entire home/apt", "private_room" => "Private room", "shared_room" => "Shared room"}
+    person_capacity   = [*1..16]
+    listing_info      = {home_style: home_style, room_style: room_style, person_capacity: person_capacity}
+    
+    listing_params = {rooms_n_beds: rooms_n_beds, listing_info: listing_info}
+    # OpenStruct for easy access
+    @listing = OpenStruct.new(listing_params)
+    
     render_wizard
   end
   
