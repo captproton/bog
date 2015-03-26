bb73fa8f692bc28f4a888679f8171aa5cdf54304
Carl Tanner <carl@wdwhub.net>
2015-03-25 18:56:01 -0700

    NF: geojson for search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,29 @@
 class SearchController < ApplicationController
+  respond_to :html, :xml, :json
   def index
-    @search = @lodging.entries
+    @search = @lodging.entries    
+    @geojson = Array.new
+
+    @search.each do |room|
+      @geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [room.longitude, room.latitude]
+        },
+        properties: {
+          name: room.title,
+          address: room.street_address,
+          :'marker-color' => '#00607d',
+          :'marker-symbol' => 'circle',
+          :'marker-size' => 'medium'
+        }
+      }
+    end
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson }
+    end    
   end
 end
